a78e09251fc044f958a7ed8bbfbeaeb5e2d80cf7
prostosava <prostosava97@gmail.com>
2023-04-25 21:24:12 +0300

    added click listeners

---
+++ logic.js
@@ -0,0 +1,17 @@
+const scalesCheckbox = document.getElementById('scales');
+scalesCheckbox.addEventListener('click', clickCheckbox1);
+
+const hornsCheckbox = document.getElementById('horns');
+hornsCheckbox.addEventListener('click', clickCheckbox2);
+
+
+function clickCheckbox1() {
+    console.log('element clicked 1');
+}
+
+function clickCheckbox2() {
+    console.log('element clicked 2');
+}
+
+
+
