cd1d58cb2c529ad1eb032a13779fe76e5acb93cf
prostosava <prostosava97@gmail.com>
2023-04-25 21:57:37 +0300

    it works

--- logic.js
+++ logic.js
@@ -2,19 +2,26 @@ const scalesCheckbox = document.getElementById('scales');
 scalesCheckbox.addEventListener('click', clickCheckbox1);
 
 const hornsCheckbox = document.getElementById('horns');
-hornsCheckbox.addEventListener('click', clickCheckbox2);
+hornsCheckbox.addEventListener('click', clickCheckbox1);
 
 const myBody = document.getElementById('my-background');
 
 
 function clickCheckbox1() {
-    myBody.style.backgroundColor = 'red'
+    if(scalesCheckbox.checked){
+         myBody.style.backgroundColor = 'red'
+    } 
+    
+    
+    if(hornsCheckbox.checked){
+         myBody.style.backgroundColor = 'blue'
+    } 
+    
+    if (!scalesCheckbox.checked && !hornsCheckbox.checked){
+         myBody.style.backgroundColor = 'green'  
+    }
 }
 
-function clickCheckbox2() {
-    myBody.style.backgroundColor = 'blue'
-
-}
 
 
 
